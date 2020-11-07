35db8fa623777aaf97d1c630a7bf30ab97cd57be
Ugo N <eagle.nnakwe@gmail.com>
2020-11-07 14:18:55 -0500

    testing hint system

--- hangman_test_files/gameplay.php
+++ hangman_test_files/gameplay.php
@@ -6,6 +6,10 @@ $gamemode = $_GET["gamemode"];
 $refresh = false;
 $mistakes = 0;
 $score = 0;
+$wordlist = wordList();
+$wordArr = pickWord($wordlist);
+$word = $wordArr[0];
+$hints = hints($wordArr);
 
 //decides whether or not the hangman refreshes
 switch($gamemode){
@@ -20,14 +24,17 @@ session_start();
 //set gameplay variables
 $_SESSION["gamemode"] = $gamemode;
 $_SESSION["score"] = $score;
-$_SESSION["word"] = "blank"; //make array of words from text file.
+$_SESSION["word"] = $word; //make array of words from text file.
+$_SESSION["hints"] = $hints;
+$_SESSION["wordlist"] = $wordlist;
 $_SESSION["mistakes"] = $mistakes;
+$_SESSION["numhints"] = 1;
 //print header
 hangmanHeader();
 
 ?>
 <br>
-This session is for <?php print $_SESSION["username"]; ?>
+This session is for <?php print $_SESSION["username"]; print "  word=" . $word; ?>
 <br>
 <?php
 hangman($mistakes);
@@ -36,7 +43,9 @@ hangman($mistakes);
 <form action="gameplay_next.php"
         method="post">    
         Guess a letter: <input name="guess" type = "text" maxlength="1" size="3"/>
-        <input type="submit" value="Guess"/>
+        <input type="submit" name="next_button" value="Guess"/>
+        <br>
+        Click for Hint: <input type="submit" name="next_button" value="Hint"/>
 </form>
 
 <?php
--- hangman_test_files/gameplay_next.php
+++ hangman_test_files/gameplay_next.php
@@ -8,25 +8,42 @@ $username = $_SESSION["username"];
 $gamemode = $_SESSION["gamemode"];
 $word = $_SESSION["word"];
 $guess = $_POST["guess"];
+$hints = $_SESSION["hints"];
+$hint = "";
+$numhints = $_SESSION["numhints"];
 
-if(!guess($guess, $word)){
-    ++$_SESSION["mistakes"];
+
+if($_POST['next_button'] == 'Guess'){
+    //$pos = strpos($word, $guess);
+    if(guess($word, $guess) === false){
+        ++$_SESSION["mistakes"];
+    }
 }
+if($_POST['next_button'] == 'Hint'){
+    if($numhints > 3){
+        $hint = "No more hints left";
+    }else{
+        $hint = $hints[$numhints];
+        $_SESSION["numhints"]++;
+    }
 
+}
 $mistakes = $_SESSION["mistakes"];
 //print header
 hangmanHeader();
 
 ?>
 <br>
-This session is for <?php print $_SESSION["username"]; ?>
+This session is for <?php print $_SESSION["username"];  print "  word=" . $word;  ?>
 <br>
 <?php hangman($mistakes); ?>
 
 <form action="gameplay_next.php"
         method="post">    
         Guess a letter: <input name="guess" type = "text" maxlength="1" size="3"/>
-        <input type="submit" value="Guess"/>
+        <input type="submit"  name="next_button" value="Guess"/>
+        <br>
+        Click for Hint: <input type="submit" name="next_button" value="Hint"/> <?php print($hint); ?>
 </form>
 
 <?php
