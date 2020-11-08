df0a38d5355624ac58d9dfa418b2d8def8339295
Ugo N <eagle.nnakwe@gmail.com>
2020-11-07 20:41:42 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include 'common.php';
+hangmanHeader();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> front page </title>
+    <link href="../styles.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <img src="../IMG/logo.svg" style="display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;align-content:center;">
+
+    <h1> Hangman game</h1>
+    <div class="front_options"><a href="signup.php">New Player<br></a></div>
+    <div class="front_options"><a href="login.php">Returning<br></a></div>
+
+    <br>
+    <p>Results and page (C) Copyright AEIOU Inc.</p>
+    <br>
+
+</body>
+
+</html>
+
+
+
+
+<?php
+hangmanFooter();
+?>
