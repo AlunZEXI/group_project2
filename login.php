e25a5cb5bac3f62894461fc7b02fc22340547cce
Ugo N <eagle.nnakwe@gmail.com>
2020-11-08 16:33:09 -0500

    css and more words

--- login.php
+++ login.php
@@ -10,13 +10,12 @@
 	$error = $_GET['err'];
 
 ?>
-<!DOCTYPE html>
 <html lang="en">
 
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Sign Up</title>
+		<title>Login</title>
 		<link rel="stylesheet" href="./hangman.css" />
 	</head>
 
