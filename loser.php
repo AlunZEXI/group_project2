1c64316fbc1e4dbb85b8c8466b80eeb091376de7
Ugo N <eagle.nnakwe@gmail.com>
2020-11-16 14:26:23 -0500

    add titles

--- loser.php
+++ loser.php
@@ -12,7 +12,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Login</title>
+		<title>Loser</title>
 		<link rel="stylesheet" href="./hangman.css" />
 	</head>
 
--- winner.php
+++ winner.php
@@ -21,7 +21,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Login</title>
+		<title>Winner</title>
 		<link rel="stylesheet" href="./hangman.css" />
 	</head>
 
